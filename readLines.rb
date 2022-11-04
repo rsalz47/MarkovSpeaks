b8a8b5a41e0ec9cf7fd6ce9cd6da4d963bc65504
rsalz47 <ronan.salz@gmail.com>
2022-11-04 00:38:32 -0400

    probability hash generation and normalization

--- readLines.rb
+++ readLines.rb
@@ -1,22 +1,22 @@
 hash = Hash.new(0)
 
-# this is all words following one specific word, not the entire hash
+# normalize probabilities of all words that follow one specific word
 def normalize(hashOfWords) 
 	tot = 0.0 # force to float
 	hashOfWords.each{|k, v| tot += v}
 	hashOfWords.each{|k, v| hashOfWords[k] /= tot}
-	puts hashOfWords
 end
 
-def buildChain
-File.readlines("test.txt").each do |line|
-	words = line.split
-	words.each_with_index do |word, idx|
-		if(idx < words.length - 1) 
-			hash[word] = Hash.new(0) if hash[word] == 0
-			hash[word][words[idx + 1]] += 1
+# build chain
+Dir.each_child("inputs") do |inp|
+	File.readlines("inputs/#{inp}").each do |line|
+		words = line.split
+		words.each_with_index do |word, idx|
+			if(idx < words.length - 1) 
+				hash[word] = Hash.new(0) if hash[word] == 0
+				hash[word][words[idx + 1]] += 1
+			end
 		end
 	end
 end
-
 hash.each{|k, v| normalize v}
